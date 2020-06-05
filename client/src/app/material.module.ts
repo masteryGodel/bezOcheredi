80887681447d3613b1d549f1ddef11ab6a336284
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-06-05 15:23:12 +0300

    feat: add styling for form

--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -2,9 +2,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { NgModule } from '@angular/core';
 @NgModule({
-  imports: [MatButtonModule, MatInputModule, MatIconModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatInputModule, MatIconModule, MatFormFieldModule],
+  imports: [MatButtonModule, MatInputModule, MatIconModule, MatFormFieldModule, MatCardModule],
+  exports: [MatButtonModule, MatInputModule, MatIconModule, MatFormFieldModule, MatCardModule],
 })
 export class MaterialModule {}
