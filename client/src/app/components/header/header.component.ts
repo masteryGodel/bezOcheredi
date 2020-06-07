1802c8bb9c5d3950e08d72013294db15d123ba2f
Gamuro <kapa6@tut.by>
2020-06-07 04:11:41 +0300

    fix: add mat-toolbar import

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 
 export class HeaderComponent {
-    constructor(public translate: TranslateService) {}
+  constructor(public translate: TranslateService) { }
 }
