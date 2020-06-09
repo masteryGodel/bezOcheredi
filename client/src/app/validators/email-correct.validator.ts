00ceb728dd1518ea3470df9b019b0ef24e2174ff
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-06-09 20:49:50 +0300

    fix: resolve regexp

--- client/src/app/validators/email-correct.validator.ts
+++ client/src/app/validators/email-correct.validator.ts
@@ -1,16 +1,16 @@
 import { FormControl } from '@angular/forms';
 
 export class EmailValidator {
-  static emailValidator(control: FormControl): { [key: string]: boolean} {
-    // tslint:disable-next-line: max-line-length
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+  static emailValidator(control: FormControl): { [key: string]: boolean } {
+    const emailRegex = new RegExp(
+      `^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)` +
+      `|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\` +
+      `.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$`
+    );
     const result = emailRegex.test(control.value);
-
     if (result) {
-
       return null;
     }
-    return {emailValidator: true};
+    return { emailValidator: true };
   }
 }
