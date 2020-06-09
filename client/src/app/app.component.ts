c2e52d5f56f8360be97df65606e78051c43deae5
Gamuro <kapa6@tut.by>
2020-06-09 10:33:21 +0300

    fix: move interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
-
-
-
-export interface Card {
-  title: string;
-  category: string;
-  description: string;
-}
-
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
